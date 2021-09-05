cadc8aaf09e2bc1a52bbeb48f12b1689f313fc23
azizul_hoq <azizulhoq4305@gamil.com>
2021-09-05 19:50:23 +0600

    tutlsp

--- Recursion.go
+++ Recursion.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
-func factorial(i int) int {
-	if i <= 4 {
-		return 4
+func fibonaci(i int) (ret int) { //fibonacci
+	if i == 0 {
+		return 0
 	}
-	return i * factorial(i-4)
+	if i == 1 {
+		return 1
+	}
+	return fibonaci(i-1) + fibonaci(i-2)
 }
 func main() {
-	var i int = 10
-	fmt.Printf("Factorial of \n %d is %d", i, factorial(i))
+	var i int
+	for i = 0; i < 10; i++ {
+		fmt.Printf("%d ", fibonaci(i))
+	}
 }
+
+// package main
+
+// import "fmt"
+
+// func factorial(i int) int {
+// 	if i <= 4 {
+// 		return 4
+// 	}
+// 	return i * factorial(i-4) //factorial
+// }
+// func main() {
+// 	var i int = 10
+// 	fmt.Printf("Factorial of \n %d is %d", i, factorial(i))
+// }
 
 // func recursion() {
 // 	recursion()
