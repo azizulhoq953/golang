4e834a631cc17deebd4009da77570c94d8d6f334
azizul_hoq <azizulhoq4305@gamil.com>
2021-09-18 18:41:18 +0600

    justclnd

--- variable.go
+++ variable.go
@@ -8,9 +8,9 @@ func main(){
 var name2 ,city string = "BARISHAL", "BANGLADESH"
 fmt.Println(name,name2,city)*/
 
-//char
+
 var c rune = 'A'
-var age int = 10
+var age int = 10           //char 
 var result float32 = 50.95
 fmt.Println(c,age,result)
 
