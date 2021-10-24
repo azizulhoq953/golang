701d080d4965c661ca1e4bac8581c81079d9c307
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-24 16:19:11 +0600

    Recom

---
+++ const.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const height = 100
+	const width = 300
+
+	area := height * width
+	fmt.Printf("Your Area Of %d", area)
+
+	//const name_to[...] float32 ={.32,64.23}
+	// const width = 50
+
+	//area := height * width
+
+	// fmt.Printf("Your string Of:%f",name)
+
+	// var Height float64
+	// var width float64
+	// fmt.Printf("Enter Area Height:")
+	// fmt.Scanf("%f", &Height)
+
+	// fmt.Printf("Enter Area width:")
+	// fmt.Scanf("%f", &width)
+
+	// area := Height * width
+	// fmt.Printf("Value Of Area:%.2f ", area)
+}
