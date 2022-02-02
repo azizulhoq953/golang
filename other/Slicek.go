913c189a53e943cf91fb7ade02db7991d12c54fd
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-02 15:05:35 +0600

    J_p

--- other/Slicek.go
+++ other/Slicek.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	company := [10]string{
-		"Azizulhoq.co", "Branch:Bangladesh and America", "Japan",
+	company := []string{
+		"Azizulhoq.co\t", "Branch:Bangladesh and America \t", "Japan \t",
 	}
 
-	company[3] = "Swithzerland"
+	company = append(company, "Append", "Append2") //append in last index
+	company[2] = "Swithzerland"
 	fmt.Println(company, "\n")
 	fmt.Println(len(company), "\n")
 	fmt.Println(cap(company))
