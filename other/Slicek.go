d0c80a5846399858fb3d90169882e60d66407404
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-02 15:15:49 +0600

    append_slice

--- other/Slicek.go
+++ other/Slicek.go
@@ -7,9 +7,15 @@ func main() {
 		"Azizulhoq.co\t", "Branch:Bangladesh and America \t", "Japan \t",
 	}
 
+	company_Earn := []string{
+		"55680", "78000", "99000",
+	}
+
+	company_all := append(company, company_Earn...)
+
 	company = append(company, "Append", "Append2") //append in last index
 	company[2] = "Swithzerland"
-	fmt.Println(company, "\n")
-	fmt.Println(len(company), "\n")
-	fmt.Println(cap(company))
+	fmt.Println(company_all, "\n")
+	fmt.Println(len(company_all), "\n")
+	fmt.Println(cap(company_all))
 }
