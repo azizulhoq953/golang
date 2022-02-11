c1a56da825eb8a73fac41aeaf30c382a3a6b63af
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-11 12:08:09 +0600

    project_cls

--- project_p/main.go
+++ project_p/main.go
@@ -3,24 +3,29 @@ package main
 import("fmt")
 
 func main(){
-/*var n int=10;
+/*
+    //int data type
+
+var n int=10;
 var m int=20;
 var sum int
 sum=n+m;
-fmt.Println(sum);*/
+fmt.Println(sum);
+*/
 
+	//string data type
 var Firstname string;
 
 var SecondName string;
 
 var name string;
 
-fmt.Scanln(&Firstname);
+fmt.Scanln(&Firstname); //usear input
 
-fmt.Scanln(&SecondName)
+fmt.Scanln(&SecondName) //usear input
 
 name=Firstname+SecondName;
 
-fmt.Printf("%v",name)
+fmt.Printf("%v",name) //print data
 
 }
