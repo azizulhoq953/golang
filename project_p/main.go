cabef649dd98dc0d222325140da4111b4569d14b
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-11 12:01:07 +0600

    project_classe

---
+++ project_p/main.go
@@ -0,0 +1,26 @@
+package main
+
+import("fmt")
+
+func main(){
+/*var n int=10;
+var m int=20;
+var sum int
+sum=n+m;
+fmt.Println(sum);*/
+
+var Firstname string;
+
+var SecondName string;
+
+var name string;
+
+fmt.Scanln(&Firstname);
+
+fmt.Scanln(&SecondName)
+
+name=Firstname+SecondName;
+
+fmt.Printf("%v",name)
+
+}
