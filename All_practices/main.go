5734931e76ef43d2295b0a6e6ad954f561dd3a35
azizulhoq953 <azizulhoq4305@gmail.com>
2022-02-15 22:24:16 +0600

    shift-linux-test

--- All_practices/main.go
+++ All_practices/main.go
@@ -1,31 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net"
-	"os"
-)
+import "fmt"
 
 func main() {
 
-	nl, err := net.Listen("tcp", ":8888") //ip :port
-
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1) //1
-	}
-	conn, err := nl.Accept() //layer -5 session ;layer
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1) //1
-	}
-
-	remoteAddr := conn.RemoteAddr().String()
-	fmt.Println(remoteAddr) //localhost ip[::1]:60941
-
-	//byte
-	conn.Write([]byte("Welcome To Our New Networking Code"))
-
-	conn.Close()
-	nl.Close()
+	fmt.Printf("Hello World")
 }
