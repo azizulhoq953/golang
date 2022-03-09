ef90ff15019021c6e3aa8864ecf761437af9e074
azizulhoq953 <azizulhoq2041@gmail.com>
2022-03-09 19:24:59 +0600

    regex-p

--- All_practices/regex.go
+++ All_practices/regex.go
@@ -1,3 +1,24 @@
+//package main
+
+// func main() {
+// 	//string type of regex
+// 	re := regexp.MustCompile(".com")
+// 	// fmt.Println(re.FindString("google.com"))
+// 	// fmt.Println(re.FindString("abc.org"))
+// 	// fmt.Println(re.FindString("fb.com"))
+// 	fmt.Println(re.FindString("google.com"))
+// 	fmt.Println(re.FindStringIndex("youtube.com"))
+// 	fmt.Println(re.FindStringSubmatch("pintarest.com"))
+
+// 	fmt.Printf("\n\n")
+
+// 	//int type of regex
+// 	sampleRegexp := regexp.MustCompile(`\d`)
+// 	fmt.Println("For regex \\d")
+// 	match := sampleRegexp.MatchString("1")
+// 	fmt.Printf("For 1: %t\n", match)
+
+// }
 package main
 
 import (
@@ -6,21 +27,10 @@ import (
 )
 
 func main() {
-	//string type of regex
-	re := regexp.MustCompile(".com")
-	// fmt.Println(re.FindString("google.com"))
-	// fmt.Println(re.FindString("abc.org"))
-	// fmt.Println(re.FindString("fb.com"))
-	fmt.Println(re.FindString("google.com"))
-	fmt.Println(re.FindStringIndex("youtube.com"))
-	fmt.Println(re.FindStringSubmatch("pintarest.com"))
-
-	fmt.Printf("\n\n")
-
-	//int type of regex
-	sampleRegexp := regexp.MustCompile(`\d`)
-	fmt.Println("For regex \\d")
-	match := sampleRegexp.MatchString("1")
-	fmt.Printf("For 1: %t\n", match)
-
+	string := "Ra13, Pa67, S-60 AC-21, UP44"
+	expression := regexp.MustCompile(`[-]?\d[\d]*[\]?[\d{2}]*`)
+	res := expression.FindAllString(string, 4)
+	for _, attribute := range res {
+		fmt.Println("The number with string is:", attribute)
+	}
 }
