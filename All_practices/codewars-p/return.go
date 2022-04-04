71ef884afce063a2a1131281881708c0867d49a3
azizulhoq953 <azizulhoq2041@gmail.com>
2022-04-04 15:21:41 +0600

    N_solve

--- All_practices/codewars-p/return.go
+++ All_practices/codewars-p/return.go
@@ -1,4 +1,4 @@
-/* explaination to problem 
+/* explaination to problem
 Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
 [Make sure you type the exact thing I wrote or the program may not execute properly]
@@ -7,11 +7,35 @@ Make a function that will return a greeting statement that uses an input; your p
 
 */
 
+// package kata
 
+// func Greet(name string) string {
+// 	// fill in solution here
+// 	return "Greet"
+// }
 
-package kata
+// func main() {
 
-func Greet(name string) string {
-  // fill in solution here
-  return ""
+// 	Greet("Hello")
+
+// }
+
+/*
+palindrome
+
+*/
+
+package main
+
+func isPalindrome(str string) bool {
+	reversedStr := ""
+	for i := len(str) - 1; i >= 0; i-- {
+		reversedStr += string(str[i])
+	}
+	for i := range str {
+		if str[i] != reversedStr[i] {
+			return false
+		}
+	}
+	return true
 }
