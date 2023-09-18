3ff109b215d4fc7272dee1a9c03cee0dfe7cf4e5
azizulhoq953 <azizulhoq2041@gmail.com>
2023-09-18 22:13:34 +0600

    usemassages

--- HtmlGo/handler/handler.go
+++ HtmlGo/handler/handler.go
@@ -1,9 +1,9 @@
 package handler
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
-
+	"text/template"
 	"github/azizulhoq953/htmltemp/render"
 )
 
@@ -15,7 +15,7 @@ func Home(w http.ResponseWriter, r *http.Request){
 	 You can use it to write the response body,
 	  set HTTP headers, and manage the response */
 
-	  render.RenderTemplate(w, "index-3.gohtml")
+	  render.RenderTemplate(w, "index-main.gohtml")
 
 }
 
@@ -30,18 +30,61 @@ func About(w http.ResponseWriter, r *http.Request){
 	           
 }
 
-func Contact(w http.ResponseWriter, r *http.Request){
 
-
-	render.RenderTemplate(w, "contact-2.gohtml")
+type Contacts struct {
+	UserName string
+	Email string
+	Message string
 }
 
 
-// func Login(w http.ResponseWriter, r *http.Request){
+func Contact(w http.ResponseWriter, r *http.Request){
+
+
+	tmpl :=template.Must(template.ParseFiles("templates/contact-2.gohtml"))
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+	details2 :=  Contacts{
+		UserName: r.FormValue("username"),
+		Email: r.FormValue("email"),
+		Message: r.FormValue("message"),
+	}
+
+	_=details2
+
+	tmpl.Execute(w, struct {SuccessMessage bool} {true} )
+	fmt.Println("User Messages:", details2)
+
+
+	// render.RenderTemplate(w, "contact-2.gohtml")
+}
+
+type LoginData struct {
+	Email string
+	Password string
+}
 
 
 
+func Login(w http.ResponseWriter, r *http.Request){
+	tmpl :=template.Must(template.ParseFiles("templates/login.gohtml"))
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+	details :=  LoginData{
+		Email: r.FormValue("email"),
+		Password: r.FormValue("password"),
+	}
+
+	_=details
+
+	tmpl.Execute(w, struct {Success bool} {true} )
+	fmt.Println("Login Informations", details)
+}
 
 
-// 	render.RenderTemplate(w, "login.gohtml")
-// }
+
+	//render.RenderTemplate(w, "login.gohtml")
--- HtmlGo/main.go
+++ HtmlGo/main.go
@@ -3,40 +3,41 @@ package main
 import(
 	"fmt"
 	"net/http"
-	"text/template"
+	// "text/template"
 	"github/azizulhoq953/htmltemp/handler"
 	// "github/azizulhoq953/htmltemp/form"
 )
 
-type Contacts struct {
-	Email string
-	Password string
-}
+// type Contacts struct {
+// 	Email string
+// 	Password string
+// }
 
 const portNumber =":8080"
 func main()  {
 
 	fs :=http.FileServer(http.Dir("assets"))
-	tmpl :=template.Must(template.ParseFiles("templates/login.gohtml"))
+	// tmpl :=template.Must(template.ParseFiles("templates/login.gohtml"))
 
 	http.HandleFunc("/",handler.Home)
 	http.HandleFunc("/about",handler.About)
 	http.HandleFunc("/contact",handler.Contact)
-	// http.HandleFunc("/login",handler.Login)
-	http.HandleFunc("/login",func(w http.ResponseWriter, r *http.Request){
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-		}
-		details :=  Contacts{
-			Email: r.FormValue("email"),
-			Password: r.FormValue("password"),
-		}
+	http.HandleFunc("/login",handler.Login)
+	// http.HandleFunc("/login",func(w http.ResponseWriter, r *http.Request){
+	// 	if r.Method != http.MethodPost {
+	// 		tmpl.Execute(w, nil)
+	// 		return
+	// 	}
+	// 	details :=  Contacts{
+	// 		Email: r.FormValue("email"),
+	// 		Password: r.FormValue("password"),
+	// 	}
 
-		_=details
+	// 	_=details
 
-		tmpl.Execute(w, struct {success bool} {true} )
-	})
+	// 	tmpl.Execute(w, struct {Success bool} {true} )
+	// 	fmt.Println("Login Informations", details)
+	// })
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	fmt.Println(fmt.Sprintf("Application Running On %s", portNumber))
