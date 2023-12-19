3cb89a6c546d01599bdf79e3a3fc5dbaa22fab12
azizulhoq953 <azizulhoq2041@gmail.com>
2023-12-19 12:28:05 +0600

    80%

--- Tl_Ecommerce/database/databasetup.go
+++ Tl_Ecommerce/database/databasetup.go
@@ -1,5 +1,6 @@
 package database
 
+//
 import (
 	"context"
 	"fmt"
@@ -11,44 +12,35 @@ import (
 )
 
 func DBSet() *mongo.Client {
-
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://azizulhoq953:azizulhoq953@cluster0.ajm4cxn.mongodb.net/?retryWrites=true&w=majority")) //mongodb://development:testpassword@localhost:27017
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
 	defer cancel()
-
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
-
 	}
 
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Println("failed to connect Mongodb:")
+		log.Println("failed to connect to mongodb")
 		return nil
 	}
-
-	fmt.Println("Successfully Connected to mongodb ")
+	fmt.Println("Successfully Connected to the mongodb")
 	return client
-
 }
 
 var Client *mongo.Client = DBSet()
 
-func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
-
-	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+func UserData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
 	return collection
+
 }
 
-func ProductData(client *mongo.client, collectionName string) *mongo.Collection {
-
-	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
-	return productCollection
+func ProductData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var productcollection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
+	return productcollection
 }
--- Tl_Ecommerce/models/models.go
+++ Tl_Ecommerce/models/models.go
@@ -10,9 +10,9 @@ type User struct {
 	ID              primitive.ObjectID `json:"_id" bson:"_id"`
 	First_Name      *string            `json:"first_name" validate:"required,min=2,max=30"`
 	Last_Name       *string            `json:"last_name"  validate:"required,min=2,max=30"`
-	Password        *string            `json:"password"   validate:"required,min=6`
-	Email           *string            `json:"email"	  validate:"email,required"`
-	Phone           *string            `json:"phone"	  validate:"required"`
+	Password        *string            `json:"password"   validate:"required,min=6"`
+	Email           *string            `json:"email"      validate:"email,required"`
+	Phone           *string            `json:"phone"      validate:"required"`
 	Token           *string            `json:"token"`
 	Refresh_Token   *string            `json:"refresh_token"`
 	Created_At      time.Time          `json:"created_at"`
@@ -28,7 +28,15 @@ type Product struct {
 	Product_Name *string            `json:"product_name" bson:"product_name"`
 	Price        *uint64            `json:"price" bson:"price"`
 	Rating       *uint8             `json:"rating" bson:"rating"`
-	Image        *string            `json:"image"bson:"image"`
+	Image        *string            `json:"image"`
+}
+
+type ProductUser struct {
+	Product_ID   primitive.ObjectID `bson:"_id"`
+	Product_Name *string            `json:"product_name" bson:"product_name"`
+	Price        int                `json:"price"  bson:"price"`
+	Rating       *uint              `json:"rating" bson:"rating"`
+	Image        *string            `json:"image"  bson:"image"`
 }
 
 type Address struct {
@@ -42,9 +50,10 @@ type Address struct {
 type Order struct {
 	Order_ID       primitive.ObjectID `bson:"_id"`
 	Order_Cart     []ProductUser      `json:"order_list" bson:"order_list"`
+	Orderered_At   time.Time          `json:"ordered_on"  bson:"ordered_on"`
 	Price          int                `json:"total_price" bson:"total_price"`
 	Discount       *int               `json:"discount" bson:"discount"`
-	Payment_Method Payment            `json:"payment_method" bson:"json:"payment_method"`
+	Payment_Method Payment            `json:"payment_method" bson:"payment_method"`
 }
 
 type Payment struct {
